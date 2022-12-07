1b50393b47499f373f64bc2c7dab698ee234348b
Pedro Henrique <pedro.henrique59@outlook.com>
2022-12-07 18:30:57 -0300

    Criando modulo tarefas

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { TarefaService } from './shared/tarefa.service';
+import { ListarTarefaComponent } from './listar/listar-tarefa.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarTarefaComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ],
+  providers: [
+    TarefaService
   ]
 })
+
 export class TarefasModule { }
